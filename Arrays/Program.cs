6ce27b9c6baaff9a556418ce5b4ebc643f37ea5e
Patrick <pat_touchette@hotmail.com>
2018-04-29 11:41:08 -0400

    master

---
+++ Arrays/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbers = new int[5];
+
+            //numbers[0] = 4;
+            //numbers[1] = 8;
+            //numbers[2] = 15;
+            //numbers[3] = 16;
+            //numbers[4] = 23;
+            ////numbers[5] = 123; will make an error
+
+
+
+            int[] numbers = new int[] { 4, 8, 15, 16, 23, 34 };
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers.Length);
+            numbers[7] = 123;
+
+            string[] names = new string[] { "eddier", "alex", "michael", "joe" };
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            string zig = "You should try to reverse this string";
+
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char ch in charArray)
+            {
+                Console.Write(ch);
+            }
+        }
+    }
+}
---
+++ HelperMethods/HelperMethods/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Name Game");
+
+            Console.Write("What's your first name? ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What's your last name? ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("In what city were you born?");
+            string city = Console.ReadLine();
+
+        
+            string reversedFirstName = ReverseString(firstName);
+            string reversedLastName = ReverseString(lastName);
+            string reversedCity = ReverseString(city);
+
+            DisplayResult(reversedFirstName, reversedLastName, reversedCity);
+            DisplayResult(reversedFirstName + reversedLastName + reversedCity);
+
+
+        }
+
+        private static string ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            return String.Concat(messageArray);
+        }
+
+        private static void DisplayResult(string reversedFirstName,
+               string reversedLastName,
+               string reversedCity)
+        {
+            Console.WriteLine("Results: ");
+            Console.Write(string.Format("{0} {1} {2}",
+            reversedFirstName,
+            reversedLastName,
+            reversedCity));
+            Console.WriteLine("\n");
+        }
+
+        private static void DisplayResult(string message)
+        {
+            Console.WriteLine("Results: ");
+            Console.Write(message);
+        }
+    }
+}
