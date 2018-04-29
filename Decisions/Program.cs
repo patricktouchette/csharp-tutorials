0e905c72ddb2eafe6673215500a811942daca121
Patrick <pat_touchette@hotmail.com>
2018-04-28 22:28:31 -0400

    some tutorials

---
+++ Decisions/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Bob's Big Giveaway");
+            //Console.WriteLine("Choose a door: 1, 2, 3: ");
+            //string userValue = Console.ReadLine();
+
+            //string message;
+
+            //if (userValue == "1")
+            //    message = "You won a new car!";
+
+            //else if (userValue == "2")
+            //    message = "You won a new boat!";
+
+            //else if (userValue == "3")
+            //    message = "You won a new cat!";
+
+            //else
+            //{
+            //    message = "Sorry we didn't understand";
+            //    message += 'You lose.'
+            //}
+            //Console.WriteLine(message);
+            //Console.ReadLine();
+
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.WriteLine("Choose a door: 1, 2, 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            Console.Write("You won a {0}.", message);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ data_types/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName = Console.ReadLine();
+        
+
+            Console.Write("Type your Last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + myFirstName + " " + myLastName);
+
+            int ss = 'a';
+            Console.WriteLine(ss);
+
+        }
+    }
+}
