138d3b4acb1a4f27d0f301cf23922226d2eb81ed
Patrick <pat_touchette@hotmail.com>
2018-05-13 02:02:48 -0400

    update

---
+++ c# Tutorial - Derek Banas/16.Threads/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _16.Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Two simple Threads
+                Thread t1 = new Thread(Print1);
+                Thread t0 = new Thread(Print0);
+
+            //t1.Start();
+            //t0.Start();
+
+            //2. Thread Sleep
+            //ThreadSleep();
+
+            //3. Thread Lock
+            BankAcct acct = new BankAcct(10);
+            Thread[] threads = new Thread[15];
+
+            Thread.CurrentThread.Name = "main";
+
+            for(int i = 0; i < 15; i++)
+            {
+                Thread t = new Thread(new ThreadStart(acct.IssueWithdraw));
+                t.Name = i.ToString();
+                threads[i] = t;
+            }
+
+            for(int i = 0; i < 15; i++)
+            {
+                Console.WriteLine( "Thread {0} Alive: {1}", threads[i].Name, threads[i].IsAlive);
+                threads[i].Start();
+                Console.WriteLine("Thread {0} Alive: {1}", threads[i].Name, threads[i].IsAlive);
+            }
+
+            Console.WriteLine("Current Priority: {0}", Thread.CurrentThread.Priority);
+            Console.WriteLine("Thread {0} Ending", Thread.CurrentThread.Name);
+
+            //4.Pass data to a thread using Lambda expressions
+            Console.WriteLine("\n\nPass data to threads");
+            Thread t2 = new Thread(() => CountTo(10));
+            t2.Start();
+
+            new Thread(() =>
+            {
+                CountTo(5);
+                CountTo(7);
+            }).Start();
+
+
+        }
+
+        //1. Two Threads
+        static void Print1()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(1);
+            }
+        }
+
+        static void Print0()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(0);
+            }
+        }
+
+        //2.Thread Sleep
+        static void ThreadSleep()
+        {
+            int num = 1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(num);
+                Thread.Sleep(1000);
+                num++;
+            }
+            Console.WriteLine("Thread Ends");
+        }
+
+        //4.Pass data to a thread using Lambda expressions
+        static void CountTo(int maxNum)
+        {
+            for(int i = 0; i <= maxNum; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    //3. Thread Lock
+    class BankAcct
+    {
+        private Object acctLock = new object();
+        double Balance { set; get; }
+
+        public BankAcct(double bal)
+        {
+            Balance = bal;
+        }
+
+        public double Withdraw(double amt)
+        {
+            if((Balance - amt) < 0)
+            {
+                Console.WriteLine($"Sorry ${Balance} in Account" );
+                return Balance;
+            }
+
+            lock (acctLock)
+            {
+                if(Balance >= amt)
+                {
+                    Console.WriteLine("Removed {0} and {1} left in Account", amt, (Balance - amt));
+                    Balance -= amt;
+                }
+                return Balance;
+            }
+        }
+
+        public void IssueWithdraw()
+        {
+            Withdraw(1);
+        }
+        
+    }
+
+}
---
+++ c# Tutorial - Derek Banas/17.File I-O/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _17.File_I_O
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo currDir = new DirectoryInfo(".");
+            Console.WriteLine(currDir.FullName);
+            Console.WriteLine(currDir.Parent);
+            Console.WriteLine(currDir.Attributes);
+            Console.WriteLine(currDir.CreationTime);
+
+            DirectoryInfo myDir = new DirectoryInfo(@"C:\Users\Pat\Documents\GitHub");
+            Console.WriteLine(myDir.Name);
+
+            //DirectoryInfo dataDir = new DirectoryInfo(@".\c#Data");
+            DirectoryInfo dataDir = new DirectoryInfo(@"C:\Users\Pat\Documents\GitHub\csharp-tutorials\c# Tutorial - Derek Banas\17.File I-O\c#Data");
+            dataDir.Create();
+
+            string[] customers =
+            {
+                "Bob Smith", "Sally Smith", "Robert Smith"
+            };
+
+            string textFilePath = @"C:\Users\Pat\Documents\GitHub\csharp-tutorials\c# Tutorial - Derek Banas\17.File I-O\c#Data\testfile1.txt";
+            File.WriteAllLines(textFilePath, customers);
+
+            foreach(string cust in File.ReadAllLines(textFilePath))
+            {
+                Console.WriteLine($"Customer : {cust}");
+            };
+
+            FileInfo[] txtFiles = dataDir.GetFiles("*.txt", SearchOption.AllDirectories);
+            Console.WriteLine("Matches: {0}", txtFiles.Length); 
+            foreach(FileInfo file in txtFiles)
+            {
+                Console.WriteLine(file.Name);
+                Console.WriteLine(file.Length);
+
+            }
+
+            //Filestream
+            string textFilePath2 = @"C:\Users\Pat\Documents\GitHub\csharp-tutorials\c# Tutorial - Derek Banas\17.File I-O\c#Data\testfile2.txt";
+            FileStream fs = File.Open(textFilePath2, FileMode.Create);
+
+            string randString = "This is a random string";
+            byte[] rsByteArray = Encoding.Default.GetBytes(randString);
+            fs.Write(rsByteArray, 0, rsByteArray.Length);
+
+            fs.Position = 0;
+
+            byte[] fileByteArray = new byte[rsByteArray.Length];
+            for(int i =0; i < rsByteArray.Length; i++)
+            {
+                fileByteArray[i] = (byte)fs.ReadByte();
+
+            }
+            Console.WriteLine(Encoding.Default.GetString(fileByteArray));
+            fs.Close();
+
+            //StreamWriter and StreamReader
+            string textFilePath3 = @"C:\Users\Pat\Documents\GitHub\csharp-tutorials\c# Tutorial - Derek Banas\17.File I-O\c#Data\testfile3.txt";
+
+            StreamWriter sw = File.CreateText(textFilePath3);
+            sw.Write("This is a random");
+            sw.WriteLine("Sentence");
+            sw.WriteLine("This is another Sentence");
+            sw.WriteLine("This is another Sentence");
+            sw.WriteLine("This is another Sentence");
+            sw.WriteLine("This is another Sentence");
+
+            sw.Close();
+            StreamReader sr = File.OpenText(textFilePath3);
+            Console.WriteLine("Peek : {0}", Convert.ToChar(sr.Peek()));
+            Console.WriteLine("1st string: {0}", sr.ReadLine());
+            Console.WriteLine("Everything: {0}", sr.ReadToEnd());
+            sr.Close();
+
+            //BinaryWriter and BinaryReader
+            string textFilePath4 = @"C:\Users\Pat\Documents\GitHub\csharp-tutorials\c# Tutorial - Derek Banas\17.File I-O\c#Data\testfile4.dat";
+
+            FileInfo datFile = new FileInfo(textFilePath4);
+            BinaryWriter bw = new BinaryWriter(datFile.OpenWrite());
+
+            string randText = "Random Text and all sorts of stuff";
+            int myAge = 28;
+            double height = 6.25;
+
+            bw.Write(randText);
+            bw.Write(myAge);
+            bw.Write(height);
+
+            bw.Close();
+
+            BinaryReader br = new BinaryReader(datFile.OpenRead());
+            Console.WriteLine(br.ReadString());
+            Console.WriteLine(br.ReadInt32());
+            Console.WriteLine(br.ReadDouble());
+            br.Close();
+
+        }
+    }
+}
