4051f470233f0898b15533299b1ae55e313b7af1
Patrick <pat_touchette@hotmail.com>
2018-05-13 23:29:11 -0400

    update

---
+++ c# Tutorial - Derek Banas/18.Serialization/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+
+namespace _18.Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Save to binary
+            Animal bowser = new Animal("Bowser", 45, 25);
+
+            Stream stream = File.Open("AnimalData.dat", FileMode.Create);
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            bf.Serialize(stream, bowser);
+            stream.Close();
+
+            bowser = null; //delete the bowser object
+
+            //Load from Binary
+            stream = File.Open("AnimalData.dat", FileMode.Open);
+
+            bf = new BinaryFormatter();
+
+            bowser = (Animal)bf.Deserialize(stream);
+            stream.Close();
+            Console.WriteLine(bowser);
+
+
+        }
+    } //program class
+
+
+    [Serializable()]
+    public class Animal : ISerializable
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public int AnimalID { get; set; }
+
+        public Animal() { }
+
+        public Animal(string name = "No Name",
+            double weight = 0,
+            double height = 0)
+        {
+            Name = name;
+            Weight = weight;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} weighs {1} lbs and is {2} inches tall",
+                Name, Weight, Height);
+        }
+
+        //Serialization function
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("Weight", Weight);
+            info.AddValue("Height", Height);
+        }
+
+        //Deserialization function
+        public Animal(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            Weight = (double)info.GetValue("Weight", typeof(double));
+            Height = (double)info.GetValue("Height", typeof(double));
+        }
+
+    } //animal class
+} // namesopace
